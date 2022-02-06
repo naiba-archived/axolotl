a8542ee25b053ee42b5fc5b7a5d4923011ad6e68
naiba <hi@nai.ba>
2022-02-06 13:13:51 +0800

    💚

--- app/frontend/src/router/index.ts
+++ app/frontend/src/router/index.ts
@@ -32,8 +32,8 @@ const router = new VueRouter({
 
 router.beforeEach(async function (from, to, next) {
   const config = await getConfig()
-  document.title = (from.meta.title ? from.meta.title + " | " : "") + config.name;
-  if (!from.meta.title) {
+  document.title = (from.meta?.title ? from.meta.title + " | " : "") + config.name;
+  if (!from?.meta?.title) {
     document.title += " - " + config.desc
   }
   next();
